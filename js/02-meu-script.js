c26aaf0948b1a77cfa76599b8f25633fd8292496
Elioise-da-casa <prevostrosie2@gmail.com>
2025-07-30 23:03:37 -0300

    meu script

---
+++ js/02-meu-script.js
@@ -0,0 +1,57 @@
+'use strict';
+'use stict';
+//Habilitando o modo estrito
+
+
+/* Exercícios */
+
+/* Crie uma variável chamada "uc" e coloque nela o número da UC que estamos*/
+
+// 2) Crie uma variável chamada "Competencia e coloque nela uma frase que representa o que estamos estudandos  nesta UC"
+
+// 3) Monte e mostre uma frase no console exibindo a UC e a competência. Exemplo: Estamos na UC2 estudando FRONT-END com JavaScript.
+
+
+
+let ucs =2;
+let competencias = "html, css, e JavaScript"
+
+console.log(`Eu estou estudando Full stack: dentro tem UC1 mais UC${ucs} e no UC${ucs}
+    tem ${competencias}`);
+
+
+    //modele 2
+
+    console.log("Eu estou estudando Full stack: dentro tem UC1 mais UC "+ucs+" e no UC"+ucs+" tem "+competencias+"");
+    
+
+
+
+let uc = 2;
+
+let competencia = "html, css, e JavaScript";
+
+
+// modelo 1
+console.log("Eu sou Elioise, e eu estou estudando  no Uc"+uc+" eu estou aprendendo "+competencia+" ");
+
+//Modelo 2
+console.log(`Eu tou estudando UC ${uc} e Eu estou aprendendo ${competencia}` );
+
+//Correto! Aspas duplas como comando, e simples como texto
+
+console.log('gosto "muito" de paçoca!');
+
+
+let frase = "Estamos estudando JavaScript na escola Senac";
+frase += "Penha, no curso de Desenvolvedor Full-Stack";
+
+frase +="no ano de "+ano;
+
+
+
+
+
+
+
+
